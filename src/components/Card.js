2b0b45c908b28fc086f1c4a4d0733f9d70056ec2
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-11-30 23:20:51 +0500

    add: button place-card__delete-icon in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,15 +16,15 @@ function Card(props) {
             <div className="place-card__description-temperature">
                 <h3 className="place-card__name">{props.card.name}</h3>
                 <p className="place-card__current-tempetature">{`${Math.round(props.card.main.temp)}°`}</p>
-                <p className="card__text">{capitalize(props.card.weather[0].description)}</p>
+                <p className="place-card__text">{capitalize(props.card.weather[0].description)}</p>
                 <p className="place-card__text">{`Ощущается как: ${Math.round(props.card.main.feels_like)}°`}</p>
             </div>
 
             <div className="place-card__description-wind">
                 <p className="place-card__text">{`Ветер:  ${props.card.wind.speed}м/с`}</p>
                 <p className="place-card__text">{`Влажность: ${props.card.main.humidity}%`}</p>
-
             </div>
+            <button className="place-card__delete-icon"></button>
 
         </div >
     );
