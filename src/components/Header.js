699ebb34056b9b081d36d5c3fd9b9d904f1faffb
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-11-30 14:28:32 +0500

    add: button class=header__button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <header className="header root__section">
             <h1 className="header__text">Weather </h1>
+            <button class="header__button">Новый прогноз</button>
         </header>
     );
 }
