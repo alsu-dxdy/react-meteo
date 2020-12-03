65e68e4b175e18d679792f6242f37a5d892b3aff
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-12-03 13:16:07 +0500

    repeat: add: AddForecastPopup with onClick=handleAddForecastSubmit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-import PopupWithForm from './components/PopupWithForm';
+import AddForecastPopup from './components/AddForecastPopup';
 import Footer from './components/Footer';
 
 import {
@@ -13,16 +13,15 @@ import './index.css';
 
 function App() {
   // Хук, управляющий внутренним состоянием.
-  const [isAddPlacePopupOpen, setisAddPlacePopupOpen] = React.useState(false);
-  // const [places, setPlaces] = React.useState([]);
+  const [isAddForecastPopupOpen, setisAddForecastPopupOpen] = React.useState(false);
   const [currentForecasts, setCurrentWeather] = React.useState([]);
 
-  function handleAddPlaceClick() {
-    setisAddPlacePopupOpen(!isAddPlacePopupOpen);
+  function handleAddForecastClick() {
+    setisAddForecastPopupOpen(!isAddForecastPopupOpen);
   }
 
   function closeAllPopups() {
-    setisAddPlacePopupOpen(false);
+    setisAddForecastPopupOpen(false);
   }
 
   React.useEffect(() => {
@@ -42,37 +41,29 @@ function App() {
         }
       )
   }, []);
+
+  function handleAddPForecastApi(data) {
+    apiCurrentWeather.getCurrentWeather(data.name)
+      .then((newForecastData) => {
+        setCurrentWeather([...currentForecasts, newForecastData]);
+        closeAllPopups()
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
+  }
+
   console.log(22);
   console.log(currentForecasts);
   return (
     <div className="root">
-      <Header onAddPlace={handleAddPlaceClick} />
+      <Header onAddForecast={handleAddForecastClick} />
       <Main currentForecasts={currentForecasts} />
 
       {      /* popup Новый прогноз */}
-      <PopupWithForm
-        title="Новый прогноз"
-        name="add-place"
-        isOpen={isAddPlacePopupOpen}
-        onClose={closeAllPopups}
-        children={
-          <form className="popup__form" name="new_place">
-            <div className="input-container">
-              <input
-                required
-                type="text"
-                name="name"
-                className="popup__input popup__input_type_name"
-                placeholder="Введите название города"
-                minLength="2"
-                maxLength="30"
-              />
-              <span className="input__error"></span>
-            </div>
+      <AddForecastPopup isOpen={isAddForecastPopupOpen} onClose={closeAllPopups}
+        onAddForecastSubmit={handleAddPForecastApi} />
 
-            <button className="button popup__button button_disabled">+</button>
-          </form>
-        } />
       <Footer />
     </div>
 
---
+++ src/components/AddForecastPopup.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PopupWithForm from './PopupWithForm';
+
+function AddForecastPopup(props) {
+    const inputNameRef = React.useRef();
+
+    function handleAddForecastSubmit(e) {
+        e.preventDefault();
+        props.onAddForecastSubmit({
+            name: inputNameRef.current.value,
+        });
+    }
+    return (
+        /* popup Новое место */
+        <PopupWithForm
+            title="Новый прогноз"
+            name="add-place"
+            isOpen={props.isOpen}
+            close={props.onClose}
+            children={
+                <form className="popup__form" name="new_place">
+                    <div className="input-container">
+                        <input
+                            required
+                            type="text"
+                            name="name"
+                            className="popup__input popup__input_type_name"
+                            placeholder="Введите название города"
+                            minLength="2"
+                            maxLength="30"
+                            ref={inputNameRef}
+                        />
+                        <span className="input__error"></span>
+                    </div>
+
+                    <button className="button popup__button button_disabled" onClick={handleAddForecastSubmit}>+</button>
+                </form>
+            } />
+
+    );
+}
+export default AddForecastPopup;
