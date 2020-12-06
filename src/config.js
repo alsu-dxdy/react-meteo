fe4af76dc6146cf9bac5e2c20542418cc77519e6
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-12-06 15:23:31 +0500

    fix: https instead http in config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const serverCurrentWeather = 'http://api.openweathermap.org/data/2.5/weather?lang=ru&units=metric&appid=23f36928bdb42e8810e88760098c476c&';
+const serverCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather?lang=ru&units=metric&appid=23f36928bdb42e8810e88760098c476c&';
 
-const serverForecact5days = 'http://api.openweathermap.org/data/2.5/forecast?lang=ru&units=metric&appid=23f36928bdb42e8810e88760098c476c&';
+const serverForecact5days = 'https://api.openweathermap.org/data/2.5/forecast?lang=ru&units=metric&appid=23f36928bdb42e8810e88760098c476c&';
 
 export {
     serverCurrentWeather,
