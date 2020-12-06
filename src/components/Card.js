47f8b6f3e5a71c355601d0289220f86221d7c7c9
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-12-06 21:12:07 +0500

    fix: remove onClick={handleClick} in div className=place-card-content in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,21 +16,21 @@ function Card(props) {
     }
 
     return (
-        <div onClick={handleClick} className="place-card">
+        <div className="place-card">
+            <div onClick={handleClick} className="place-card-content">
+                <div className="place-card__description-temperature">
+                    <h3 className="place-card__name">{name}</h3>
+                    <p className="place-card__current-tempetature">{`${Math.round(main.temp)}°`}</p>
+                    <p className="place-card__text">{capitalize(weather[0].description)}</p>
+                    <p className="place-card__text">{`Ощущается как: ${Math.round(main.feels_like)}°`}</p>
+                </div>
 
-            <div className="place-card__description-temperature">
-                <h3 className="place-card__name">{name}</h3>
-                <p className="place-card__current-tempetature">{`${Math.round(main.temp)}°`}</p>
-                <p className="place-card__text">{capitalize(weather[0].description)}</p>
-                <p className="place-card__text">{`Ощущается как: ${Math.round(main.feels_like)}°`}</p>
-            </div>
-
-            <div className="place-card__description-wind">
-                <p className="place-card__text">{`Ветер:  ${Math.round(wind.speed)}м/с`}</p>
-                <p className="place-card__text">{`Влажность: ${main.humidity}%`}</p>
+                <div className="place-card__description-wind">
+                    <p className="place-card__text">{`Ветер:  ${Math.round(wind.speed)}м/с`}</p>
+                    <p className="place-card__text">{`Влажность: ${main.humidity}%`}</p>
+                </div>
             </div>
             <button onClick={() => removeForecast(id)} className="place-card__delete-icon"></button>
-
         </div >
     );
 }
